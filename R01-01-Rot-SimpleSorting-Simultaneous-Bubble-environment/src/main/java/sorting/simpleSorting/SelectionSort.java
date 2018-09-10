db6f7ca525e6794d40532c9a85a983d04bf8f6d2
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2018-09-10 16:04:30 -0300

    first commit

---
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/SelectionSort.java
@@ -0,0 +1,27 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * The selection sort algorithm chooses the smallest element from the array and
+ * puts it in the first position. Then chooses the second smallest element and
+ * stores it in the second position, and so on until the array is sorted.
+ */
+public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+   @Override
+   public void sort(T[] array, int leftIndex, int rightIndex) {
+      for (int i = leftIndex; i < rightIndex; i++) {
+         int min = i;
+         for (int j = i + 1; j < rightIndex + 1; j++) {
+            if (array[j].compareTo(array[min]) < 0) {
+               min = j;
+            }
+         }
+         Util.swap(array, i, min);
+      }
+
+      //throw new UnsupportedOperationException("Not Implemented yet!");
+   }
+}
