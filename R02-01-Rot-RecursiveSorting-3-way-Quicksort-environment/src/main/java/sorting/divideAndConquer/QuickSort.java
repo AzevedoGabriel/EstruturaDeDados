533c0d7d72d084f9ac921152f9db6801c7ee2992
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2018-09-10 22:41:33 -0300

    :/

--- R02-01-Rot-RecursiveSorting-3-way-Quicksort-environment/src/main/java/sorting/divideAndConquer/QuickSort.java
+++ R02-01-Rot-RecursiveSorting-3-way-Quicksort-environment/src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -30,8 +30,8 @@ public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
    private int partition(T[] array, int leftIndex, int rightIndex) {
       T pivot = array[leftIndex];
       int i = leftIndex;
-      for (int j = leftIndex + 1; j < rightIndex; j++) {
-         if (array[j].compareTo(pivot) < 0) {
+      for (int j = leftIndex + 1; j <= rightIndex; j++) {
+         if (array[j].compareTo(pivot) <= 0) {
             i += 1;
             Util.swap(array, i, j);
 
